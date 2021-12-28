c5bd8a927320867009377e39df321dd7069afaea
lxhao61 <lxhao61@gmail.com>
2021-12-28 17:28:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,15 +11,37 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.6.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 删除原版softethervpn插件
 rm -rf feeds/packages/net/softethervpn
 
-# 拉取softethervpn升级插件
-svn co https://github.com/lxhao61/packages-1/trunk/net/softethervpn feeds/packages/net/softethervpn
+# 拉取Lean大softethervpn插件
+svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn feeds/packages/net/softethervpn
+
+# 删除原版softethervpn5插件
+rm -rf feeds/packages/net/softethervpn5
+
+# 拉取Lean大softethervpn5插件
+svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
+
+# 删除原版luci-app-softethervpn插件
+rm -rf feeds/lienol/luci-app-softethervpn
+
+# 拉取修改后的luci-app-softethervpn插件
+svn co https://github.com/lxhao61/openwrt-package/trunk/luci-app-softethervpn feeds/lienol/luci-app-softethervpn
+
+# 删除自带xray-core核心
+rm -rf package/feeds/packages/xray-core
+
+# 拉取passwall源码
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
+
+# 删除passwall中xray-core核心
+#rm -rf package/passwall/xray-core
 
 # kenzok8大整理的插件包（含lienol大的passwall与lean大的ssr-plus等）
 #git clone https://github.com/kenzok8/openwrt-packages.git package/lienol
