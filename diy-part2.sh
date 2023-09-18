5ee26d03f7c211721219d489193ec6c83c5448f5
lxhao61 <lxhao61@gmail.com>
2023-09-18 08:51:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,17 +15,23 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/192.168.1.1/192.168.6.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 删除lienol大自带xray-core核心
-rm -rf package/feeds/packages/xray-core
-
 # 拉取PassWall源码
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
-cd package/xiaorouji/packages
-git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
-cd -
-#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
-#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/passwall/packages
+#cd package/passwall/packages
+#git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
+#cd -
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/passwall/luci
+#cd package/passwall/luci
+#git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
+#cd -
+
+# 拉取ShadowSocksR Plus+源码
+#git clone -b master https://github.com/fw876/helloworld.git package/helloworld
+
+# 拉取phtunnel、pgyvpn源码
+git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
 
 # 删除原版softethervpn插件
 #rm -rf feeds/packages/net/softethervpn
