2ca42ac3b33156624dff22a82f107fb136dd8211
lxhao61 <lxhao61@gmail.com>
2023-12-15 00:12:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,41 +14,37 @@
 #sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 拉取PassWall源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/passwall/packages
-#cd package/passwall/packages
+# 删除自带 xray-core 源码
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+
+# 拉取 PassWall 源码
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
+#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/feeds/packages/passwall
+#cd package/feeds/packages/passwall
 #git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 #cd -
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/passwall/luci
-#cd package/passwall/luci
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/feeds/luci/luci-app-passwall
+#cd package/feeds/luci/luci-app-passwall
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取ShadowSocksR Plus+源码
-#git clone -b master https://github.com/fw876/helloworld.git package/helloworld
+# 拉取 ShadowSocksR Plus+ 源码
+#git clone -b master https://github.com/fw876/helloworld.git package/feeds/helloworld
 
-# 拉取phtunnel、pgyvpn源码
-git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
+# 拉取 phtunnel、pgyvpn 源码
+#git clone https://github.com/OrayOS/OpenOray.git package/feeds/OpenOray
 
-# 删除原版softethervpn插件
-#rm -rf feeds/packages/net/softethervpn
+# 提取 phtunnel 源码
+svn co https://github.com/coolsnowwolf/packages/trunk/net/phtunnel package/feeds/packages/phtunnel
 
-# 拉取Lean大softethervpn插件
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn feeds/packages/net/softethervpn
+# 提取 luci-app-phtunnel 源码
+svn co https://github.com/OrayOS/OpenOray/trunk/luci-app-phtunnel package/feeds/luci/luci-app-phtunnel
 
-# 删除原版softethervpn5插件
-#rm -rf feeds/packages/net/softethervpn5
-
-# 拉取Lean大softethervpn5插件
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
-
-# 删除原版luci-app-softethervpn插件
-#rm -rf feeds/lienol/luci-app-softethervpn
-
-# 拉取修改后的luci-app-softethervpn插件
-#svn co https://github.com/lxhao61/openwrt-package/trunk/luci-app-softethervpn package/lienol/luci-app-softethervpn
+# 拉取 msd_lite 源码
+git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
 
 # lean插件相关
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipv6-helper package/lean/ipv6-helper
