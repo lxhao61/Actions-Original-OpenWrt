718face456d3a01ab55b29215b10bc07af0ba0d7
lxhao61 <lxhao61@gmail.com>
2024-01-19 15:51:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,11 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 拉取 luci-app-tailscaler 源码
-git clone https://github.com/Carseason/openwrt-tailscale.git package/feeds/luci/luci-app-tailscaler
+git clone https://github.com/selfcan/luci-app-tailscale.git package/feeds/luci/luci-app-tailscale
+#git clone https://github.com/LeanFly/luci-app-tailscale.git package/feeds/luci/luci-app-tailscale
+
+# 拉取 luci-app-tailscaler 源码
+#git clone https://github.com/Carseason/openwrt-tailscale.git package/feeds/luci/luci-app-tailscaler
 
 # 拉取 immortalwrt openwrt-23.05 源码
 git clone -b openwrt-23.05 https://github.com/immortalwrt/immortalwrt.git iwrt23
