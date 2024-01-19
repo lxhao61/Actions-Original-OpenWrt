fe1abc08316f72f4683fac08da331bffcfe1fbed
lxhao61 <lxhao61@gmail.com>
2024-01-19 21:41:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,46 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
+# 删除自带 golang 源码
+#rm -rf feeds/packages/lang/golang
+
+# 拉取 golang 源码
+#git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+
+# 删除自带 hysteria 源码
+#rm -rf feeds/packages/net/hysteria
+#rm -rf package/feeds/packages/hysteria
+
+# 删除自带 v2ray-geodata 源码
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf package/feeds/packages/v2ray-geodata
+
+# 删除自带 xray-core 源码
+rm -rf feeds/packages/net/xray-core
+rm -rf package/feeds/packages/xray-core
+
+# 删除自带 luci-app-passwall 源码
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf package/feeds/luci/luci-app-passwall
+
+# 拉取 PassWall 源码
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
+#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/feeds/packages/passwall
+#cd package/feeds/packages/passwall
+#git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
+#cd -
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/feeds/luci/luci-app-passwall
+#cd package/feeds/luci/luci-app-passwall
+#git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
+#cd -
+
+# 拉取 ShadowSocksR Plus+ 源码
+#git clone -b master https://github.com/fw876/helloworld.git package/feeds/helloworld
+
+# 拉取 phtunnel、pgyvpn 源码
+#git clone https://github.com/OrayOS/OpenOray.git package/feeds/OpenOray
+
 # 拉取 immortalwrt openwrt-23.05 源码
 git clone -b openwrt-23.05 https://github.com/immortalwrt/immortalwrt.git iwrt23
 # 提取 fullconenat-nft 源码
