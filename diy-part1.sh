87f7393470717b53c7996011790d5ce16517ee95
lxhao61 <lxhao61@gmail.com>
2024-01-22 15:49:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,16 +28,16 @@ git checkout v22.03.6
 # 注释掉默认 packages 源
 #sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
 
-# 注释掉默认 luci 源
-#sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
-
 # 添加 packages 源
 #sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' feeds.conf.default
 
+# 注释掉默认 luci 源
+sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
+
 # 添加 luci 源
+sed -i '$a src-git luci https://github.com/lxhao61/luci.git;openwrt-22.03' feeds.conf.default
 #sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;22.03' feeds.conf.default
 
 # 添加 lienol 大的 package 源
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git;main' feeds.conf.default
 #sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
-sed -i '$a src-git msd_lite https://github.com/CHDman/luci-app-msd_lite.git;main' feeds.conf.default
