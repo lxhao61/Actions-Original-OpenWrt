8d1cb37a3f9724aafe4b4b68a1472491573a5542
lxhao61 <lxhao61@gmail.com>
2024-01-25 03:49:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,51 +21,54 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i "s/timezone='.*'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/.*timezone='CST-8'.*/i\ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
-# 删除自带 golang 源码
+# 删除自带 golang
 rm -rf feeds/packages/lang/golang
 
-# 拉取 golang 源码
+# 拉取 golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
-# 删除自带 xray-core 源码
+# 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
 
-# 删除自带 autosamba 源码
+# 删除自带 autosamba
 rm -rf feeds/other/lean/autosamba
 rm -rf package/feeds/other/autosamba
 
-# 删除自带 luci-app-turboacc 源码
+# 删除自带 luci-app-turboacc
 rm -rf feeds/other/lean/luci-app-turboacc
 rm -rf package/feeds/other/luci-app-turboacc
 
-# 删除自带 luci-app-samba 源码
+# 删除自带 luci-app-samba
 rm -rf feeds/luci/applications/luci-app-samba
 rm -rf package/feeds/luci/luci-app-samba
 
-# 拉取 passwall-packages 源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
-#cd package/feeds/packages/passwall
+# 拉取 passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall
+#cd package/passwall/packages
 #git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 #cd -
 
-# 拉取 luci-app-passwall 源码
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
-#cd package/feeds/luci/luci-app-passwall
+# 拉取 luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#cd package/passwall/luci-app-passwall
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取 ShadowSocksR Plus+ 源码
+# 拉取 ShadowSocksR Plus+
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
 
-# 拉取 msd_lite 源码
+# 拉取 msd_lite
 git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
 git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
 
-# 拉取 phtunnel、pgyvpn 源码
+# 拉取 phtunnel、pgyvpn
 #git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
 
-# 删除自带 tailscale 源码
+# 删除 passwall-packages 中 naiveproxy
+rm -rf package/passwall/naiveproxy
+
+# 删除自带 tailscale
 rm -rf feeds/packages/net/tailscale
 #rm -rf package/feeds/packages/tailscale
 
@@ -88,5 +91,7 @@ function merge_package(){
     done
     cd "$rootdir"
 }
-# 提取 tailscale 源码
-merge_package main https://github.com/kenzok8/small-package feeds/packages/net/tailscale tailscale
+# 提取 naiveproxy
+merge_package master https://github.com/immortalwrt/packages.git package/passwall/naiveproxy net/naiveproxy
+# 提取 tailscale
+merge_package main https://github.com/kenzok8/small-package.git feeds/packages/net/tailscale tailscale
