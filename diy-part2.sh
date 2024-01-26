c230988154638fb1635ccd3ba04b231099868fe8
lxhao61 <lxhao61@gmail.com>
2024-01-26 08:18:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,13 +21,13 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i "s/timezone='.*'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "/.*timezone='CST-8'.*/i\ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
-# 删除自带 golang 源码
-#rm -rf feeds/packages/lang/golang
+# 删除自带 golang
+rm -rf feeds/packages/lang/golang
 
-# 拉取 golang 源码
-#git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+# 拉取 golang
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
-# 删除自带 xray-core 源码
+# 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
 
@@ -43,5 +43,45 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取 ShadowSocksR Plus+ 源码
-#git clone -b master https://github.com/fw876/helloworld.git package/feeds/helloworld
+# 拉取 ShadowSocksR Plus+
+#git clone -b master https://github.com/fw876/helloworld.git package/helloworld
+
+# 拉取 msd_lite
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
+
+# 拉取 phtunnel、pgyvpn
+#git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
+
+# 删除 passwall-packages 中 naiveproxy
+rm -rf package/passwall/packages/naiveproxy
+
+# 删除自带 tailscale
+rm -rf feeds/packages/net/tailscale
+
+# 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+# 提取 naiveproxy
+merge_package master https://github.com/immortalwrt/packages.git package/passwall/packages net/naiveproxy
+# 提取 phtunnel、luci-app-phtunnel
+merge_package master https://github.com/coolsnowwolf/packages.git package/phtunnel net/phtunnel
+merge_package main https://github.com/OrayOS/OpenOray.git package/phtunnel luci-app-phtunnel
+# 提取 tailscale
+merge_package main https://github.com/kenzok8/small-package.git feeds/packages/net tailscale
