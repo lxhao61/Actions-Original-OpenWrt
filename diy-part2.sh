6af585c95f733841c137b9160775af141945c36a
lxhao61 <lxhao61@gmail.com>
2024-01-26 08:20:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,10 +22,10 @@ sed -i "s/timezone='.*'/timezone='CST-8'/g" package/base-files/files/bin/config_
 sed -i "/.*timezone='CST-8'.*/i\ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 
 # 删除自带 golang
-rm -rf feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
 
 # 拉取 golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 # 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
