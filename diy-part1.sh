882dc06c60dfc2c610226441c47fd76b594be772
lxhao61 <lxhao61@gmail.com>
2024-02-04 11:17:58 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,19 +20,16 @@ git checkout v22.03.6
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 注释默认 packages
 sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
-
 # 添加 packages
 sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git;22.03' feeds.conf.default
 
 # 注释默认 luci
 sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
-
 # 添加 luci
 sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;22.03' feeds.conf.default
 
