2ad7c43e3390942eec8f1dcc6b25d9de6934ea24
lxhao61 <lxhao61@gmail.com>
2024-02-20 03:39:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -52,9 +52,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pas
 
 # 拉取 luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
-cd package/passwall/luci
-git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
-cd -
+#cd package/passwall/luci
+#git checkout ebd3355bdf2fcaa9e0c43ec0704a8d9d8cf9f658
+#cd -
 
 # 拉取 ShadowSocksR Plus+
 #git clone https://github.com/fw876/helloworld.git -b master package/helloworld
