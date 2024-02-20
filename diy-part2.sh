14281004df2d8407639ed01b5b30a2803e0ec35e
lxhao61 <lxhao61@gmail.com>
2024-02-20 19:49:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -68,6 +68,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 # 删除自带 ddns-scripts
 rm -rf feeds/packages/net/ddns-scripts
+# 删除自带 dnsmasq
+rm -rf package/network/services/dnsmasq
 # 删除自带 haproxy
 #rm -rf feeds/packages/net/haproxy
 # 删除 passwall-packages 中 gn
@@ -102,6 +104,8 @@ function merge_package(){
 }
 # 提取 ddns-scripts
 merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/packages/net net/ddns-scripts
+# 提取 dnsmasq
+merge_package 22.03 https://github.com/Lienol/openwrt.git package/network/services package/network/services/dnsmasq
 # 提取 haproxy
 #merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/packages/net net/haproxy
 # 提取 gn
