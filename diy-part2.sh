3437b2de0e5f079f85eea9d5c5373349b923a453
lxhao61 <lxhao61@gmail.com>
2024-02-21 22:55:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -60,8 +60,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 #git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 
 # 拉取 msd_lite、luci-app-msd_lite
-#git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
-#git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
 
 # 拉取 OpenAppFilter、luci-app-oaf
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
@@ -69,7 +69,7 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # 删除自带 ddns-scripts
 rm -rf feeds/packages/net/ddns-scripts
 # 删除自带 dnsmasq
-#rm -rf package/network/services/dnsmasq
+rm -rf package/network/services/dnsmasq
 # 删除 passwall-packages 中 gn
 rm -rf package/passwall/packages/gn
 # 删除 passwall-packages 中 naiveproxy
@@ -103,7 +103,7 @@ function merge_package(){
 # 提取 ddns-scripts
 merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/packages/net net/ddns-scripts
 # 提取 dnsmasq
-#merge_package 22.03 https://github.com/Lienol/openwrt.git package/network/services package/network/services/dnsmasq
+merge_package 22.03 https://github.com/Lienol/openwrt.git package/network/services package/network/services/dnsmasq
 # 提取 gn
 merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git package/passwall/packages devel/gn
 # 提取 naiveproxy
@@ -115,4 +115,4 @@ merge_package openwrt-23.05 https://github.com/immortalwrt/packages.git feeds/pa
 # 提取 luci-app-socat
 merge_package main https://github.com/chenmozhijin/luci-app-socat.git feeds/lienol luci-app-socat
 # 提取 msd_lite、luci-app-msd_lite
-merge_package main https://github.com/kenzok8/small-package.git package/msd_lite msd_lite luci-app-msd_lite
+#merge_package main https://github.com/kenzok8/small-package.git package/msd_lite msd_lite luci-app-msd_lite
