f9a407251c655e9d505d23bacb73c9fe23a88e5a
haoran <lxhao61@gmail.com>
2024-08-31 13:32:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -47,7 +47,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 # 删除自带 hysteria
 rm -rf feeds/packages/net/hysteria
 # 删除自带 naiveproxy
-rm -rf feeds/packages/net/naiveproxy
+#rm -rf feeds/packages/net/naiveproxy
 # 删除自带 v2ray-geodata
 rm -rf feeds/packages/net/v2ray-geodata
 # 删除自带 xray-core
@@ -81,7 +81,7 @@ merge_package main https://github.com/xiaorouji/openwrt-passwall-packages.git fe
 #merge_package v5 https://github.com/sbwml/openwrt_helloworld.git feeds/packages/net hysteria
 # 提取 naiveproxy
 #merge_package master https://github.com/immortalwrt/packages.git feeds/packages/net net/naiveproxy
-merge_package v5 https://github.com/sbwml/openwrt_helloworld.git feeds/packages/net naiveproxy
+#merge_package v5 https://github.com/sbwml/openwrt_helloworld.git feeds/packages/net naiveproxy
 # 提取 v2ray-geodata
 merge_package main https://github.com/xiaorouji/openwrt-passwall-packages.git feeds/packages/net v2ray-geodata
 # 提取 xray-core
